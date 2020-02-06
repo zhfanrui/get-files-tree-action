ffc549f4f4e6f5ec85dda71ea4b8179c67379949
zhfanrui <i@zhfanrui.com>
2020-02-06 19:09:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,5 @@ git commit -m 'get tree'
 remote_repo="https://$GITHUB_ACTOR:${ github.token }@github.com/$GITHUB_REPOSITORY.git"
 echo $remote_repo
 echo "https://$GITHUB_ACTOR@github.com/$GITHUB_REPOSITORY.git"
+echo $ACTIONS_RUNTIME_TOKEN
 git push --force $remote_repo master:tree
