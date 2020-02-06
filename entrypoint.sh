06e9d34dcf908dbd0fb135d01eddd1f097b3c76d
zhfanrui <i@zhfanrui.com>
2020-02-06 18:50:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -l
 
-remote_repo="https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-echo $remote_repo
-
 git init
 git config user.name "$GITHUB_ACTOR"
 git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 wget https://api.github.com/repos/$GITHUB_REPOSITORY/contents
 git add .
 git commit -m 'get tree'
+
+
+remote_repo="https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+echo $remote_repo
 git push --force $remote_repo master:tree
